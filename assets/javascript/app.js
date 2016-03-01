10b48dbab4e8c7ef13ed9bc312358b8e8d8f1554
Jason Martocci <Jason Martocci>
2016-03-01 13:15:58 -0500

    got answers to hide and show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
   var correct = 0;
   var incorrect = 0;
   var unanswered = 0;
+  var value = "";
 
 window.onload = function(){
   $('#stop').click(stopwatch.stop);
@@ -13,12 +14,23 @@ window.onload = function(){
 
 function questionOne(){
   $("#question").append("<h3>What is two plus two?</h3>");
-  $("#answerOne").append("<button type='submit' class='submit' name='question1' value='incorrect'>5</button><br>");
-  $("#answerTwo").append("<button type='submit' class='submit' name='question1' value='incorrect'>9</button><br>");
-  $("#answerThree").append("<button type='submit' class='submit' name='question1' value='incorrect'>3</button><br>");
-  $("#answerFour").append("<button type='submit' class='submit' name='question1' value='incorrect'>8</button><br>");
-  $("#answerFive").append("<button type='submit' class='submit' name='question1' value='correct'>4</button><br>");
+  $("#answerOne").append("<button type='submit' class='submit' name='question1' value='incorrect' id='myBtnOne'>5</button><br>");
+  $("#answerTwo").append("<button type='submit' class='submit' name='question1' value='incorrect' id='myBtnTwo'>9</button><br>");
+  $("#answerThree").append("<button type='submit' class='submit' name='question1' value='incorrect' id='myBtnThree'>3</button><br>");
+  $("#answerFour").append("<button type='submit' class='submit' name='question1' value='incorrect' id='myBtnFour'>8</button><br>");
+  $("#answerFive").append("<button type='submit' class='submit' name='question1' value='correct' id='myBtnFive'>4</button><br>");
 
+    $("button").click(function(){
+        var ans = $(this);
+        if(ans.val() === "correct"){
+          correct++;
+          alert('You are correct');
+        }else if(ans.val() === "incorrect"){
+          incorrect++;
+        }else{
+          unanswered++;
+        }
+    }); 
 
     $(".submit").click(function(){
       questionTwo();
@@ -26,19 +38,67 @@ function questionOne(){
 }
 
 function questionTwo(){
-  $("#question").replaceWith("<h3>What is three plus two?</h3>");
-  $("#answerOne").replaceWith("<button type='submit' class='submit' name='question1' value='correct'>5</button><br>");
-  $("#answerTwo").replaceWith("<button type='submit' class='submit' name='question1' value='incorrect'>7</button><br>");
-  $("#answerThree").replaceWith("<button type='submit' class='submit' name='question1' value='incorrect'>2</button><br>");
-  $("#answerFour").replaceWith("<button type='submit' class='submit' name='question1' value='incorrect'>9</button><br>");
-  $("#answerFive").replaceWith("<button type='submit' class='submit' name='question1' value='incorrect'>3</button><br>");
+  $("#question").empty().append("<h3>What is three plus two?</h3>");
+  $("#answerOne").empty().append("<button type='submit' class='submit' name='question2' value='correct'>5</button><br>");
+  $("#answerTwo").empty().append("<button type='submit' class='submit' name='question2' value='incorrect'>7</button><br>");
+  $("#answerThree").empty().append("<button type='submit' class='submit' name='question2' value='incorrect'>2</button><br>");
+  $("#answerFour").empty().append("<button type='submit' class='submit' name='question2' value='incorrect'>9</button><br>");
+  $("#answerFive").empty().append("<button type='submit' class='submit' name='question2' value='incorrect'>3</button><br>");
 
+    $("button").click(function(){
+        var ans = $(this);
+        if(ans.val() === "correct"){
+          correct++;
+          alert('You are correct');
+        }else if(ans.val() === "incorrect"){
+          incorrect++;
+        }else{
+          unanswered++;
+        }
+    }); 
 
     $(".submit").click(function(){
-      alert("Game Over");
+      questionThree();
     });
 }
 
+function questionThree(){
+  $("#question").empty().append("<h3>What is the color brown?</h3>");
+  $("#answerOne").empty().append("<button type='submit' class='submit' name='question3' value='correct'>5</button><br>");
+  $("#answerTwo").empty().append("<button type='submit' class='submit' name='question3' value='incorrect'>7</button><br>");
+  $("#answerThree").empty().append("<button type='submit' class='submit' name='question3' value='incorrect'>2</button><br>");
+  $("#answerFour").empty().append("<button type='submit' class='submit' name='question3' value='incorrect'>9</button><br>");
+  $("#answerFive").empty().append("<button type='submit' class='submit' name='question3' value='incorrect'>3</button><br>");
+
+    $("button").click(function(){
+        var ans = $(this);
+        if(ans.val() === "correct"){
+          correct++;
+          alert('You are correct');
+        }else if(ans.val() === "incorrect"){
+          incorrect++;
+        }else{
+          unanswered++;
+        }
+    }); 
+
+    $(".submit").click(function(){
+      results();
+    });
+}
+
+function results(){
+
+    $("#question").empty().append("<h3>Correct Answers: " + correct + "</h3>");
+    $("#answerOne").empty().append("<h3>Incorrect Answers: " + incorrect + "</h3>");
+    $("#answerTwo").empty().append("<h3>Unanswered: " + unanswered + "</h3>");
+    $("#answerThree").empty().append("");
+    $("#answerFour").empty().append("");
+    $("#answerFive").empty().append("");
+
+  }
+
+// Timer function
 var stopwatch = {
   time:30,
   reset: function(){
@@ -77,7 +137,6 @@ var stopwatch = {
 };
 
 function stop(){
-	debugger;
   //clearInterval(counter);
 }
 
